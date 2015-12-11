a2950219d376b73f3604f82df3dc2008e2c04ed6
nadim <i.nadim@gmail.com>
2015-12-11 00:27:40 -0500

    some style fix

--- tutorial.rb
+++ tutorial.rb
@@ -3,7 +3,7 @@ require 'em-hiredis'
 require 'thin'
 require 'sinatra'
 
-EventMachine.run do
+EM.run do
 
   clients = []
 
@@ -11,16 +11,18 @@ EventMachine.run do
   redis.pubsub.subscribe("timeseries-data") do |msg|
     clients.each{ |client| client.send(msg)}
   end
-  #websocket
+ 
   EM::WebSocket.run(:host => "0.0.0.0", :port => 3002) do |ws|
     ws.onopen { |handshake| clients << ws}
     ws.onclose { clients.delete ws}  
   end
-  #webserver
+
   class App < Sinatra::Base
     get '/' do send_file 'index.html' end
   end
+
   Thin::Server.start App, '0.0.0.0', 3000
-  trap "INT" do EventMachine.stop_event_loop end  
+
+  trap "INT" do EM.stop_event_loop end  
 end
 
