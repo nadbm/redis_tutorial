9bb646f3b7b40eafc9c3e05d685ef70141f19295
Nadim Islam <nislam@vigilantglobal.com>
2015-12-11 11:51:53 -0500

    better data

--- data-aggregator.rb
+++ data-aggregator.rb
@@ -5,10 +5,13 @@ EM.run do
   redis = EM::Hiredis.connect
 
   color = ENV['COLOR'] || 'green'
+  value = 5
   EM::PeriodicTimer.new(0.5) do
-    value = rand.rand(0..0.5)
+    value += rand.rand(-0.5..0.5)
     redis.set("timeseries-data:#{color}", value).callback do
       redis.publish("timeseries-data:#{color}", value)
     end
   end
+
+  trap "INT" do EM.stop_event_loop end  
 end
